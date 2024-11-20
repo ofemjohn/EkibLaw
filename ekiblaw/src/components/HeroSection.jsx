a0f880419458617f70775bdf20583f67aef962f7
johnteacher <newuser@MacBook-Air.local>
2024-11-19 19:34:38 -0500

    updated the logo to an svg asset

--- ekiblaw/src/components/HeroSection.jsx
+++ ekiblaw/src/components/HeroSection.jsx
@@ -66,8 +66,8 @@ const HeroSection = () => {
                   ':hover': {
                     backgroundColor: 'rgba(239, 174, 100, 0.9)',
                   },
-                  width: { xs: '155.5px', md: '245px' },
-                  height: { xs: '45px', md: '72px' },  
+                  width: { xs: '200px', md: '245px' }, 
+                  height: { xs: '60px', md: '72px' },  
                   alignSelf: { xs: 'center', md: 'flex-start' },
                   fontFamily: 'Teachers',
                 }}
